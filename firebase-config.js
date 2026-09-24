5cb41020220ff56bb40a0da4bb9d7f468840e716
fauzifaturohman <fauzifaturohman@gmail.com>
2026-09-24 10:41:42 +0700

    Update Firebase configuration details
    Updated Firebase configuration with new API key and identifiers.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // firebase-config.js
 // ⚠️ JANGAN commit file ini ke repo publik. Tambahkan ke .gitignore.
 export const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXX",
+  apiKey: "AIzaSyA2XIRkCAHjTY4oD4q0CHLExO21BXgXs8Y",
   authDomain: "siap-sr-39-garut.firebaseapp.com",
   projectId: "siap-sr-39-garut",
-  storageBucket: "siap-sr-39-garut.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  storageBucket: "siap-sr-39-garut.firebasestorage.app",
+  messagingSenderId: "475082566497",
+  appId: "1:475082566497:web:e42a538131dedcbabdb320"
 };
