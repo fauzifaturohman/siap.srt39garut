3c548f105e9a6fd31111f93e3ee9e060430a25be
fauzifaturohman <fauzifaturohman@gmail.com>
2026-09-24 10:16:10 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+// ⚠️ JANGAN commit file ini ke repo publik. Tambahkan ke .gitignore.
+export const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "siap-sr-39-garut.firebaseapp.com",
+  projectId: "siap-sr-39-garut",
+  storageBucket: "siap-sr-39-garut.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456"
+};
